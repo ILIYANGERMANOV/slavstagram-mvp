38851e208dd2a1461b5f44b5976c7186a817af1f
ILIYANGERMANOV <iliyan.germanov971@gmail.com>
2017-05-21 20:59:57 +0300

    Setup project base: -create MainActivity.java -add CORE dependencies: ButterKnife, GSON and Firebase -move versions to root project build.gradle

---
+++ Slavstagram/app/src/main/java/com/blin1/slavstagram/screens/main/MainActivity.java
@@ -0,0 +1,39 @@
+package com.blin1.slavstagram.screens.main;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.blin1.slavstagram.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.butter_knife_test_text_view)
+    TextView mTestTextView;
+
+    public static void startScreen(@NonNull Context context) {
+        Intent intent = new Intent(context, MainActivity.class);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setup();
+    }
+
+    private void setup() {
+        ButterKnife.bind(this);
+        setupUI();
+    }
+
+    private void setupUI() {
+        mTestTextView.setText(mTestTextView.getText() + " 100% working!");
+    }
+}
--- Slavstagram/app/src/main/java/com/blin1/slavstagram/screens/splash/SplashActivity.java
+++ Slavstagram/app/src/main/java/com/blin1/slavstagram/screens/splash/SplashActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.blin1.slavstagram.screens.main.MainActivity;
+
 /**
  * Created by iliyan on 21.05.17.
  */
@@ -13,5 +15,6 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        MainActivity.startScreen(this);
     }
 }
