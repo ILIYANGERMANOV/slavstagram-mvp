e1c0607b00a6f4afda0773683a15020cad3e431e
ILIYANGERMANOV <iliyan.germanov971@gmail.com>
2017-06-11 15:34:39 +0300

    Make image preview actionbar's home button works

--- Slavstagram/app/src/main/java/com/babushka/slav_squad/ui/screens/image_preview/ImagePreviewActivity.java
+++ Slavstagram/app/src/main/java/com/babushka/slav_squad/ui/screens/image_preview/ImagePreviewActivity.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.babushka.slav_squad.GlideApp;
 import com.babushka.slav_squad.R;
@@ -63,5 +64,15 @@ public class ImagePreviewActivity extends AppCompatActivity {
         if (mImageUrl == null) {
             throw new IllegalArgumentException("ImagePreviewActivity: '" + EXTRA_IMAGE_URL + "' is required!");
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            // Respond to the action bar's Up/Home button
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
